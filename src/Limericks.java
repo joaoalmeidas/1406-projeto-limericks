d96cda8341eb304fd0588d924a2059e49f2335e9
joao <joao182cx@gmail.com>
2019-02-19 07:40:29 -0300

    Mudanças no código. Definição dos seis substantivos que farão a rima nos versos 1, 2, 5.

--- src/Limericks.java
+++ src/Limericks.java
@@ -6,56 +6,37 @@ public class Limericks {
 		
 		
 		final String[] artigo = {"the", "a", "one", "some", "any"};
-		final String[] substantivo = {"boy", "girl", "dog", "town", "car", "toy", "joy", "cowboy", "playboy", "soy", "earl", "pearl", "burl",
-				"log", "fog", "bog", "blog", "crown", "brown", "gown", "frown", "renown", "bar", "star", "scar", "guitar", "cigar", "czar", "tsar",
+		final String[] substantivo = {"boy", "girl", "dog", "town", "car", "toy", "joy", "cowboy", "playboy", "soy", "earl", "pearl", "burl", "knurl",
+				"swirl","log", "fog", "bog", "blog", "sandhog", "crown", "brown", "gown", "frown", "renown", "bar", "star", "scar", "guitar", "cigar", "czar", "tsar",
 				"char", "bazar"};
 		final String[] verbo = {"drove", "jumped", "ran", "walked", "skipped"};
 		final String[] preposicao = {"to", "from", "over", "under", "on"};
 		
 		String[] limericks = new String[5];
-		String rima1 = "";
-		String rima2 = "";
-		
+		String[] rima1 = new String[6];
+		String[] rima2 = new String[4];
 		
 		SecureRandom aleatorio = new SecureRandom();
 		
-		rima1 = substantivo[aleatorio.nextInt(substantivo.length)];
+		rima1[0] = substantivo[aleatorio.nextInt(substantivo.length)];
 		
-		do{
+		for(int i = 1; i < rima1.length; i++) {
 			
-			rima2 = substantivo[aleatorio.nextInt(substantivo.length)];
+			do {
+				
+				rima1[i] = substantivo[aleatorio.nextInt(substantivo.length)];
+				
+			}while(!rima1[i-1].substring(rima1[i-1].length()-2, rima1[i-1].length()).equals(rima1[i].substring(rima1[i].length()-2, rima1[i].length())) || rima1[i].equals(rima1[i-1]));
 			
-		}while(rima1.equals(rima2));
+		}
+		
+		for(String a: rima1) {
+			System.out.println(a);
+		}
 		
 		
 			
-		String frase = "";
-		
-		frase += artigo[aleatorio.nextInt(artigo.length)] + " ";
-		frase = frase.substring(0, 1).toUpperCase() + frase.substring(1, frase.length());
-		frase += substantivo[aleatorio.nextInt(substantivo.length)] + " ";
-		frase += verbo[aleatorio.nextInt(verbo.length)] + " ";
-		frase += preposicao[aleatorio.nextInt(preposicao.length)] + " ";
-		frase += artigo[aleatorio.nextInt(artigo.length)] + " ";
-		frase += rima1 + ",";
-		
-		limericks[0] = frase;
-			
-		
-		System.out.println(limericks[0]);
-		
-		frase = "";
-		frase += artigo[aleatorio.nextInt(artigo.length)] + " ";
-		frase = frase.substring(0, 1).toUpperCase() + frase.substring(1, frase.length());
-		frase += substantivo[aleatorio.nextInt(substantivo.length)] + " ";
-		frase += verbo[aleatorio.nextInt(verbo.length)] + " ";
-		frase += preposicao[aleatorio.nextInt(preposicao.length)] + " ";
-		frase += artigo[aleatorio.nextInt(artigo.length)] + " ";
-		frase += rima2 + ",";
-		
-		limericks[2] = frase;
-		
-		System.out.println(limericks[2]);
+
 
 	}
 
